903d8ecc096dc2e28f60cd426226ac09f13e1369
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-25 18:40:11 +0700

    register login

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:client_dlaw/common/style.dart';
 import 'package:client_dlaw/ui/home_page.dart';
+import 'package:client_dlaw/ui/login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class SplashScreen extends StatelessWidget {
       splash: Image.asset('assets/logo_splash.png', width: 400, height: 400),
       splashIconSize: 400,
       duration: 1500,
-      nextScreen: const HomePage(),
+      nextScreen: const LoginScreen(),
       splashTransition: SplashTransition.fadeTransition,
       // make gradient background color
       backgroundColor: backgroundColor1,
