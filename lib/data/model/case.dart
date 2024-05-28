7a04f3f10d55c3e94021900ed807f2cd8158e513
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-28 20:59:08 +0700

    Update case.dart

--- lib/data/model/case.dart
+++ lib/data/model/case.dart
@@ -6,8 +6,7 @@ class Case {
   late String media;
   late String notes;
   late String status;
-  late num consultationFee;
-  late num hourlyFee;
+  late num hour;
   late num additionFee;
   late String lawyerId;
   late String clientId;
@@ -18,8 +17,7 @@ class Case {
     required this.media,
     required this.notes,
     required this.status,
-    required this.consultationFee,
-    required this.hourlyFee,
+    required this.hour,
     required this.additionFee,
     required this.lawyerId,
     required this.clientId,
@@ -31,10 +29,52 @@ class Case {
     media = json['media'];
     notes = json['notes'];
     status = json['status'];
-    consultationFee = json['consultationFee'];
-    hourlyFee = json['hourlyFee'];
+    hour = json['hour'];
     additionFee = json['additionFee'];
     lawyerId = json['lawyerId'];
     clientId = json['clientId'];
   }
 }
+
+var CaseList = [
+  Case(
+      id: 'CA01',
+      subject: 'Divourse Case',
+      media: 'logo.png',
+      notes: 'Court Date 10-02-2024',
+      status: 'Completed',
+      hour: 10,
+      additionFee: 10,
+      lawyerId: 'L01',
+      clientId: 'C01'),
+  Case(
+      id: 'CA02',
+      subject: 'Plastic Case Coca Cola',
+      media: 'logo.png',
+      notes: 'None',
+      status: 'Reviewing',
+      hour: 0,
+      additionFee: 0,
+      lawyerId: 'L01',
+      clientId: 'C05'),
+  Case(
+      id: 'CA03',
+      subject: 'Inheritance Split',
+      media: 'logo.png',
+      notes: 'None',
+      status: 'Reviewing',
+      hour: 0,
+      additionFee: 0,
+      lawyerId: 'L02',
+      clientId: 'C04'),
+  Case(
+      id: 'CA04',
+      subject: 'Evironmental Case',
+      media: 'logo.png',
+      notes: 'None',
+      status: 'Approved',
+      hour: 10,
+      additionFee: 20,
+      lawyerId: 'L03',
+      clientId: 'C03')
+];
