a6b52461f85040c0128c1ab22ecefe94ef98871c
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-29 14:14:07 +0700

    half home

--- lib/ui/components/home.dart
+++ lib/ui/components/home.dart
@@ -29,22 +29,91 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          leading: null,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  onFilterButtonPressed();
+                },
+                icon: const Icon(Icons.person_2_outlined)),
+            IconButton(
               onPressed: () {
                 onFilterButtonPressed();
               },
-              icon: Icon(Icons.person_2_outlined)),
-          IconButton(
-            onPressed: () {
-              onFilterButtonPressed();
-            },
-            icon: Icon(Icons.notifications_none_outlined),
-          )
-        ],
-      ),
-    );
+              icon: const Icon(Icons.notifications_none_outlined),
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+                padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                child: Text(
+                  "Welcome Back!",
+                  style: TextStyle(fontSize: 30),
+                )),
+            Stack(children: <Widget>[
+              Image.asset(
+                "images/back-home.png",
+                width: 800,
+                fit: BoxFit.cover,
+              ),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(40, 40, 20, 20),
+                  child: Text(
+                    "Try a free consultation",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(40, 70, 20, 20),
+                  child: Text(
+                    "Find a suitable service for your legal needs",
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 95, 20, 20),
+                  child: SizedBox(
+                      width: 100,
+                      height: 45,
+                      child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 0),
+                          child: FloatingActionButton(
+                            backgroundColor: Color.fromARGB(255, 241, 223, 63),
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0))),
+                            onPressed: () {
+                              onFilterButtonPressed();
+                            },
+                            child: const Text(
+                              "Learn More",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(0.0, 0.0),
+                                    blurRadius: 3.0,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ))))
+            ]),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                child: Text(
+                  "Discover",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ))
+          ],
+        ));
   }
 }
